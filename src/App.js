c9bb9d4aa2fcda8311b93e5961b6b9efadcad622
anja <rasoloanja@gmail.com>
2020-07-01 22:29:47 -0400

    Improved validation of input -  Ensured that the user cannot enter empty strings by adding space

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ class App extends Component {
   }
 
   validateInput = (value) => {
-    if(value.length > 2){
+    if(value.trim().length > 2){
       return true
     } else {
       return false
